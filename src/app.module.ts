dfddb49d7e0a3ca02fb8a58ba96fc7fb7a2f7f34
antero <antero2203@gmail.com>
2023-02-13 00:50:29 -0500

    antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 
@@ -20,6 +21,8 @@ import { CommonModule } from './common/common.module';
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
   ], 
 })
 export class AppModule {}
